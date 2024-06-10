9036ce1ee920cb83fdf327b32105a41f8a3d43aa
centDiglesia <vincent.iglesia07@gmail.com>
2024-06-10 23:17:34 +0800

    dfdsf

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace sprout__gradeBook
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Teacher_Dashboard("Vincent7070"));
+            Application.Run(new introductionPage());
         }
     }
 }
